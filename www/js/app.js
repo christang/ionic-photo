11ffdec35219b36d4c3d536b9de6b77f867296b5
Chris Tang <chris.l.tang@gmail.com>
2015-12-28 22:42:10 -0500

    Demo: ionic firebase
    https://www.firebase.com/docs/web/libraries/ionic/guide.html

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'firebase'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,18 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+.factory("Items", function($firebaseArray) {
+  var itemsRef = new Firebase("https://incandescent-fire-4990.firebaseio.com/items");
+  return $firebaseArray(itemsRef);
+})
+.controller("ListCtrl", function($scope, Items) {
+  $scope.items = Items;
+  $scope.addItem = function() {
+    var name = prompt("What do you need to buy?");
+    if (name) {
+      $scope.items.$add({
+        "name": name
+      });
+    }
+  };
+});
