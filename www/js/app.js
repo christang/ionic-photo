4e5dfa01118d6ca7fd406c98abe4bf867b65723d
Chris Tang <chris.l.tang@gmail.com>
2015-12-29 20:09:43 -0500

    Add login/register/secure views

--- www/js/app.js
+++ www/js/app.js
@@ -22,6 +22,21 @@ angular.module('starter', ['ionic', 'firebase', 'ngCordova'])
     }
   });
 })
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state("firebase", {
+      url: "/firebase",
+      templateUrl: "templates/firebase.html",
+      controller: "FirebaseController",
+      cache: false
+    })
+    .state("secure", {
+      url: "/secure",
+      templateUrl: "templates/secure.html",
+      controller: "SecureController"
+    });
+  $urlRouterProvider.otherwise('/firebase');
+});
 .factory("FbUrl", function () {
   return "incandescent-fire-4990";
 })
@@ -32,6 +47,67 @@ angular.module('starter', ['ionic', 'firebase', 'ngCordova'])
 .factory("Items", function($firebaseArray, FbUrl) {
   var itemsRef = new Firebase("https://" + FbUrl + ".firebaseio.com/items");
   return $firebaseArray(itemsRef);
+})
+.controller("FirebaseController", function($scope, $state, $firebaseAuth) {
+  var fbAuth = $firebaseAuth(fb);
+
+  $scope.login = function(username, password) {
+    fbAuth.$authWithPassword({
+      email: username,
+      password: password
+    }).then(function(authData) {
+      $state.go("secure");
+    }).catch(function(error) {
+      console.error("ERROR: " + error);
+    });
+  }
+
+  $scope.register = function(username, password) {
+    fbAuth.$createUser({email: username, password: password}).then(function(userData) {
+      return fbAuth.$authWithPassword({
+        email: username,
+        password: password
+      });
+    }).then(function(authData) {
+      $state.go("secure");
+    }).catch(function(error) {
+      console.error("ERROR: " + error);
+    });
+  }
+})
+.controller("SecureController", function($scope, $ionicHistory, $firebaseArray, $cordovaCamera) {
+  $ionicHistory.clearHistory();
+  $scope.images = [];
+
+  var fbAuth = fb.getAuth();
+  if(fbAuth) {
+    var userReference = fb.child("users/" + fbAuth.uid);
+    var syncArray = $firebaseArray(userReference.child("images"));
+    $scope.images = syncArray;
+  } else {
+    $state.go("firebase");
+  }
+
+  $scope.upload = function() {
+    var options = {
+      quality : 75,
+      destinationType : Camera.DestinationType.DATA_URL,
+      sourceType : Camera.PictureSourceType.CAMERA,
+      allowEdit : true,
+      encodingType: Camera.EncodingType.JPEG,
+      popoverOptions: CameraPopoverOptions,
+      targetWidth: 500,
+      targetHeight: 500,
+      saveToPhotoAlbum: false
+    };
+    $cordovaCamera.getPicture(options).then(function(imageData) {
+      syncArray.$add({image: imageData}).then(function() {
+        alert("Image has been uploaded");
+      });
+    }, function(error) {
+      console.error(error);
+    });
+  }
 })
 .controller("ListCtrl", function($scope, Items) {
   $scope.items = Items;
